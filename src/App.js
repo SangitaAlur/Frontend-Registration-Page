563e8002389975ac24505bb9a8f2d93bcbf2f2c4
SangitaAlur <sangeetalalur@gmail.com>
2024-06-08 19:53:21 +0530

    Frontend of RegistrationPage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import EmployeeUpload from './Components/EmployeeUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="mt-4">
+        <h1>Registration</h1>
+      </header>
+      <EmployeeUpload/>
+    </div>
+  );
+}
+
+export default App;
